66789ae979a2380bd371b8284d5eb9e0cb14e9b3
Nguyễn Minh Duy <>
2021-02-03 00:34:54 +0700

    structured best folder angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardLayoutComponent } from './core/layouts/dashboard-layout/dashboard-layout.component';
+import { ALL_ROUTES } from './core/routes/all-routes';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: DashboardLayoutComponent,
+    children: ALL_ROUTES
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
