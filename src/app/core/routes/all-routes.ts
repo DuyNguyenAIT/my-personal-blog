316b961314d07b94644e74984880643dc810939d
Nguyễn Minh Duy <>
2021-02-04 00:37:29 +0700

    no anything

--- src/app/core/routes/all-routes.ts
+++ src/app/core/routes/all-routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from "@angular/router";
-import { NotFound404Component } from "../components/not-found404/not-found404.component";
 
 export const ALL_ROUTES: Routes = [
   { path: "", loadChildren: () => import('../../home/home.module').then(m => m.HomeModule) },
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 @NgModule({
-  declarations: [AuthRoutingModule.components],
+  declarations: [AuthRoutingModule.components, ForgotPasswordComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
