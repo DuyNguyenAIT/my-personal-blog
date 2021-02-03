4d46daeff3decdba336bbaa2f59c9027cfb68485
Nguyễn Minh Duy <>
2021-02-03 13:25:15 +0700

    scroll to top completed

--- src/app/core/components/scoll-to-top/scoll-to-top.component.ts
+++ src/app/core/components/scoll-to-top/scoll-to-top.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-scoll-to-top',
@@ -7,8 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScollToTopComponent implements OnInit {
 
-  constructor() { }
-
+  windowScrolled!: boolean;
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+      if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+          this.windowScrolled = true;
+      } 
+     else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+          this.windowScrolled = false;
+      }
+  }
+  scrollToTop() {
+      (function smoothscroll() {
+          var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+          if (currentScroll > 0) {
+              window.requestAnimationFrame(smoothscroll);
+              window.scrollTo(0, currentScroll - (currentScroll / 8));
+          }
+      })();
+  }
+  
   ngOnInit(): void {
   }
 
