ea21b7ae7899ce16b59888f562a7c90fb1fa3b2c
Nguyễn Minh Duy <>
2021-02-06 19:37:35 +0700

    no message

---
+++ src/app/core/models/category.ts
@@ -0,0 +1,10 @@
+export class Category {
+    
+    categoryId?: number;
+
+    categoryName!: string;
+
+    enable: boolean = false;
+
+    createdAt!: Date;
+}
---
+++ src/app/core/models/subcategory.ts
@@ -0,0 +1,10 @@
+export class SubCategory {
+    
+    subCategoryId?: number;
+
+    categoryId?: number;
+
+    subCategoryName!: string;
+
+    createdAt?: Date;
+}
